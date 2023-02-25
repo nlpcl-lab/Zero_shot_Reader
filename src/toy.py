9c5cd1b1a3919784fc42d01fe0cff49c694baede
zomss <nelllpic@kaist.ac.kr>
2023-02-25 23:46:40 +0900

    Revise

--- src/toy.py
+++ src/toy.py
@@ -1,5 +1,6 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
 from torch.utils.data import DataLoader, Dataset
+from src.util import _normalize_answer
 import pytorch_lightning as pl
 import json
 from torch import nn
@@ -24,27 +25,37 @@ class QASDataset(Dataset):
         return self.context[i], self.question[i], self.answer[i]
 
 
+
 class Reader(pl.LightningModule):
     def __init__(self, args):
         super().__init__()
-        self.model = AutoModelForCausalLM.from_pretrained(args.model, cache_dir="/home/tjrals/models")
+        self.model = AutoModelForCausalLM.from_pretrained(args.model, cache_dir="./models/")
         self.tokenizer = AutoTokenizer.from_pretrained(args.model)
         self.template = "{d}\nQuestion: {q}\nAnswer: "
-        self.batch = args.batch
+        self.batch_size = args.batch
 
     def forward(self, input):
-        generated_ids = self.model.generate(**input, max_length=512)
+        generated_ids = self.model.generate(**input, max_length=input.input_ids.shape[1]+10)
         return generated_ids
 
-    def predict_step(self, batch, batch_idx):
+
+    def test_step(self, batch, batch_idx):
         texts = [self.template.format(d=d, q=q) for d,q in zip(batch[0], batch[1])]
         input = self.tokenizer(texts, padding=True, return_tensors="pt").to(self.device)
         generated_ids = self(input)
         preds = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
-        print(preds)
-        exit(0)
+        preds = [_normalize_answer(p.split("Answer: ")[-1]) for p in preds]
+        answers = [_normalize_answer(a) for a in batch[2][0]]
+        acc = self._accuracy(answers, preds)
+        self.log("Test_Acc", acc,batch_size=self.batch_size)
+
+
+    def _accuracy(self, golds, preds):
+        total = len(golds)
+        cor = sum([int(g in p) for g, p in zip(golds, preds)])
+        return cor/total
 
     def get_dataloader(self, data_path):
         dataset = QASDataset(data_path)
-        return DataLoader(dataset, batch_size=int(self.batch))
+        return DataLoader(dataset, batch_size=int(self.batch_size))
 
--- test.py
+++ test.py
@@ -3,6 +3,8 @@ import logging
 from beir import LoggingHandler
 import pytorch_lightning as pl
 from src.toy import QASDataset, Reader
+from pytorch_lightning.loggers import CSVLogger
+from pytorch_lightning.callbacks.progress import TQDMProgressBar
 
 def timestr():
     return time.strftime("%Y%m%d-%H%M%S")
@@ -14,7 +16,7 @@ def parse():
     parser.add_argument("--dataset", type=str, default="nq")
     parser.add_argument("--split", type=str, default='test')
     parser.add_argument("--dataset_dir", type=str, default="./data/")
-    parser.add_argument("--batch", type=int, default=2)
+    parser.add_argument("--batch", type=int, default=1)
 
     #Reader
     parser.add_argument("--model", type=str, default="facebook/opt-6.7b")
@@ -37,8 +39,8 @@ def main():
     data_path = "./data/nq/nq-dev.json"
     dataloader = model.get_dataloader(data_path)
 
-    trainer = pl.Trainer(accelerator="gpu", devices=1)
-    trainer.predict(model, dataloaders=dataloader)
+    trainer = pl.Trainer(accelerator="gpu", devices=1, logger=CSVLogger(save_dir="./logs/"), callbacks=[TQDMProgressBar(refresh_rate=10)])
+    trainer.test(model, dataloaders=dataloader)
 
 
 if __name__=="__main__":
