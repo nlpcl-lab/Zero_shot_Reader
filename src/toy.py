c778a4108ae150203fc5b84728666016b5ef2b59
zomss <nelllpic@kaist.ac.kr>
2023-02-26 16:41:07 +0900

    Revsie

--- src/toy.py
+++ src/toy.py
@@ -1,4 +1,5 @@
 from transformers import AutoTokenizer, AutoModelForCausalLM
+from transformers import T5Tokenizer, T5ForConditionalGeneration
 from torch.utils.data import DataLoader, Dataset
 from src.util import _normalize_answer
 import pytorch_lightning as pl
@@ -29,13 +30,23 @@ class QASDataset(Dataset):
 class Reader(pl.LightningModule):
     def __init__(self, args):
         super().__init__()
-        self.model = AutoModelForCausalLM.from_pretrained(args.model, cache_dir="./models/")
-        self.tokenizer = AutoTokenizer.from_pretrained(args.model)
-        self.template = "{d}\nQuestion: {q}\nAnswer: "
+        self.model_name = args.model
+        if "T0" not in args.model:
+            self.model = AutoModelForCausalLM.from_pretrained(args.model, cache_dir="./models/")
+            self.tokenizer = AutoTokenizer.from_pretrained(args.model)
+            self.template = "Passage: {d}\nQuestion: {q}\nAnswer: "
+        else:
+            self.model = T5ForConditionalGeneration.from_pretrained("./models/T0_3B")
+            self.tokenizer = T5Tokenizer.from_pretrained("./models/T0_3B")
+            self.template = "Please answer the question based on the passage.\n\nPassage: {d}\n\nQuestion: {q}"
         self.batch_size = args.batch
 
     def forward(self, input):
-        generated_ids = self.model.generate(**input, max_length=input.input_ids.shape[1]+10)
+        if 'T0' not in self.model_name:
+            max_length = input.input_ids.shape[1]+10
+        else:
+            max_length = 10
+        generated_ids = self.model.generate(input.input_ids, max_length=max_length,return_dict_in_generate=True, output_scores=True)
         return generated_ids
 
 
@@ -43,7 +54,7 @@ class Reader(pl.LightningModule):
         texts = [self.template.format(d=d, q=q) for d,q in zip(batch[0], batch[1])]
         input = self.tokenizer(texts, padding=True, return_tensors="pt").to(self.device)
         generated_ids = self(input)
-        preds = self.tokenizer.batch_decode(generated_ids, skip_special_tokens=True)
+        preds = self.tokenizer.batch_decode(generated_ids.sequences, skip_special_tokens=True)
         preds = [_normalize_answer(p.split("Answer: ")[-1]) for p in preds]
         answers = [_normalize_answer(a) for a in batch[2][0]]
         acc = self._accuracy(answers, preds)
